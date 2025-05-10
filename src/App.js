643762b7b1c2f201b77e56c8d284178ca298e3dd
Tawab <tawabsarhadi111@gmail.com>
2025-05-10 21:06:45 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Posts from "./components/Posts";
+import Navbar from "./components/BlogNav"
+const App = () => {
+    return (
+        <div className="main-container" style={{backgroundColor: "aliceblue"}}>
+            <Navbar />
+            <Posts />
+        </div>
+    );
+};
+
+export default App;
